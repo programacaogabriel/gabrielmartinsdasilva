990ec31debfdad91eb26199bd011fe359d9254af
programacaogabriel <programacaogabriel@gmail.com>
2022-12-21 23:39:08 -0300

    :lipstick: Animacao
    Efeito maquina de escrever no animacao_nome

--- scripts_/script.js
+++ scripts_/script.js
@@ -1,14 +1,11 @@
-var alturaElemento = $(window).height();
+// animação do nome
+function typeWriter(el) {
+    const textArray = el.innerHTML.split('');
+    el.innerHTML = '';
+    textArray.forEach((letter, i) =>
+        setTimeout(() => (el.innerHTML += letter), 95 * i)
+    );
 
-function alturaSecao() {
-    $('.secao').height(alturaElemento);
 }
 
-$(alturaSecao);
-const $header = document.querySelector('header')
-
-
-
-
-
-
+typeWriter(animacao_nome);
